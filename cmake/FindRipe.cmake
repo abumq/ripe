40bfbca9f893533bdbc515ffbd44cec6522e127c
mkhan <mkhan@allhomes.com.au>
2017-02-03 14:56:35 +1100

    header cmake

--- cmake/FindRipe.cmake
+++ cmake/FindRipe.cmake
@@ -3,9 +3,9 @@ set(RIPE_PATHS ${RIPE_ROOT} $ENV{RIPE_ROOT})
 find_path(RIPE_INCLUDE_DIR
         Ripe.h
         PATH_SUFFIXES include
-        PATHS ${Ripe_PATHS}
+        PATHS ${RIPE_PATHS}
 )
 
 include(FindPackageHandleStandardArgs)
 
-find_package_handle_standard_args(Ripe REQUIRED_VARS Ripe_INCLUDE_DIR)
+find_package_handle_standard_args(Ripe REQUIRED_VARS RIPE_INCLUDE_DIR)
