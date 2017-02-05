0dccd90a1bed577fc376405bc55d03fec7462e19
Majid Khan <mkhan3189@gmail.com>
2017-02-05 12:05:05 +1100

    --leak-check=full

--- .travis-build.sh
+++ .travis-build.sh
@@ -13,15 +13,15 @@ echo "313004c475a3986d2034e77542ab1d5b:123:EM+2WPE9fXxrna+Pyb0Ycw==" | valgrind 
 echo
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> RSA Keypair Generation"
 echo
-valgrind ./ripe -g --rsa --out-private private.pem --out-public public.pem
+valgrind --leak-check=full ./ripe -g --rsa --out-private private.pem --out-public public.pem
 echo
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> RSA Encryption"
 echo
-echo 'plain text' | valgrind ./ripe -e --rsa --in-key public.pem --out output.enc
+echo 'plain text' | valgrind --leak-check=full ./ripe -e --rsa --in-key public.pem --out output.enc
 echo
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> RSA Decryption"
 echo
-valgrind ./ripe -d --rsa --in-key private.pem --in output.enc --base64
+valgrind --leak-check=full ./ripe -d --rsa --in-key private.pem --in output.enc --base64
 echo
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Base64 Encoding"
 echo
