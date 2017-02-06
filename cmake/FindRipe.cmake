ede11dbb8df5061ddd26321219b2f85163080c13
Majid <mkhan3189@gmail.com>
2017-02-06 15:42:03 +1100

    Update FindRipe.cmake

--- cmake/FindRipe.cmake
+++ cmake/FindRipe.cmake
@@ -1,10 +1,16 @@
+# Finds Ripe headers and OpenSSL as dependency
+# Creates ${RIPE_INCLUDE_DIR}
 set(RIPE_PATHS ${RIPE_ROOT} $ENV{RIPE_ROOT})
 
+# Check for OpenSSL
+find_package (openssl REQUIRED)
+include_directories (${OPENSSL_INCLUDE_DIR})
+
 find_path(RIPE_INCLUDE_DIR
-        Ripe.h
-		RipeHelpers.h
-        PATH_SUFFIXES include
-        PATHS ${RIPE_PATHS}
+    Ripe.h
+    RipeHelpers.h
+    PATH_SUFFIXES include
+    PATHS ${RIPE_PATHS}
 )
 
 find_library(RIPE_LIBRARY
