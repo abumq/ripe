e2ce912c9ab7230fea7631be605bd7473b6f0b6a
Majid Khan <mkhan3189@gmail.com>
2017-08-19 14:21:25 +1000

    some more updates to ripe5

--- include/Ripe5.h
+++ include/Ripe5.h
@@ -3,7 +3,10 @@
 #include <map>
 #include <string>
 #include <vector>
-#include "include/InfInt.h"
+#include "include/Ripe.h"
+//#include "include/InfInt.h"
+#include <cryptopp/osrng.h>
+#include <cryptopp/integer.h>
 
 ///
 /// \brief Ripe class
@@ -13,14 +16,20 @@
 ///
 class Ripe5 {
 public:
-    typedef InfInt BigInteger;
+    typedef CryptoPP::Integer BigInteger; // this is only temp as InfInt does not support shifting of bits
     typedef long long BigIntegerOld;
+    typedef long long int byte_t;
+
+    enum class PaddingType {
+        PKCS_1v1_5,
+        NO_PADDING
+    };
 
     class PublicKey {
     public:
         PublicKey() = default;
 
-        PublicKey(BigInteger n, unsigned int e) :
+        PublicKey(BigInteger n, int e) :
             m_n(n),
             m_e(e) {
         }
@@ -28,18 +37,18 @@ public:
         virtual ~PublicKey() = default;
 
         inline BigInteger n() const { return m_n; }
-        inline unsigned int e() const { return m_e; }
+        inline int e() const { return m_e; }
 
     private:
         BigInteger m_n;
-        unsigned int m_e;
+        int m_e;
     };
 
     class RawKey {
     public:
         static const unsigned int DEFAULT_PUBLIC_EXPONENT = 65537;
 
-        RawKey(BigInteger p, BigInteger q, unsigned int e = DEFAULT_PUBLIC_EXPONENT, bool skipPrimeCheck = true) :
+        RawKey(BigInteger p, BigInteger q, int e = DEFAULT_PUBLIC_EXPONENT, bool skipPrimeCheck = true) :
             m_p(p),
             m_q(q),
             m_e(e)
@@ -76,7 +85,7 @@ public:
         inline BigInteger q() const { return m_q; }
         inline BigInteger coeff() const { return m_coeff; }
         inline BigInteger n() const { return m_n; }
-        inline unsigned int e() const { return m_e; }
+        inline int e() const { return m_e; }
         inline BigInteger d() const { return m_d; }
         inline BigInteger dp() const { return m_dq; }
         inline BigInteger dq() const { return m_dp; }
@@ -109,7 +118,7 @@ public:
     private:
         BigInteger m_p;
         BigInteger m_q;
-        unsigned int m_e;
+        int m_e;
         BigInteger m_coeff;
         BigInteger m_n;
         BigInteger m_d;
@@ -133,37 +142,115 @@ public:
         PublicKey m_publicKey;
     };
 
-    static long long int encrypt(const PublicKey* publicKey, long long int m)
-    {
-        return powerMod(BigInteger(m), publicKey->e(), publicKey->n());
+    static std::map<unsigned long, int> pkcs1pad2(const std::string& s, unsigned long n) {
+        if (n < s.size() + 11) { // TODO: fix for utf-8
+            throw std::runtime_error("Message too long for RSA");
+        }
+        std::map<unsigned long, int> ba; // TODO: Use int for key too?? as this is bit length
+        long long i = s.size() - 1;
+        while(i >= 0 && n > 0) {
+            int c = static_cast<int>(s.at(i--));
+            if (c < 128) { // encode using utf-8
+                ba[--n] = c;
+            }
+            else if ((c > 127) && (c < 2048)) {
+                ba[--n] = (c & 63) | 128;
+                ba[--n] = (c >> 6) | 192;
+            }
+            else {
+                ba[--n] = (c & 63) | 128;
+                ba[--n] = ((c >> 6) & 63) | 128;
+                ba[--n] = (c >> 12) | 224;
+            }
+        }
+        ba[--n] = 0;
+        int r = rand() % 100 + 1;
+
+        srand (time(NULL));
+        while(n > 2) { // random non-zero pad
+            r = 0;
+            while(r == 0) r = rand() % 100 + 1;
+            ba[--n] = r;
+        }
+        ba[--n] = 2;
+        ba[--n] = 0;
+        return ba;
     }
 
-    static long long int decrypt(const PrivateKey* privateKey, long long int m)
-    {
-        return powerMod(BigInteger(m), privateKey->d(), privateKey->n());
+    static std::string pkcs1unpad2(BigInteger m, unsigned long n) {
+        byte b[n];
+        m.Encode(b, n);
+        /*
+        var b = d.toByteArray();
+        var i = 0;
+        while(i < b.length && b[i] == 0) ++i;
+        if(b.length-i != n-1 || b[i] != 2)
+            return null;
+        ++i;
+        while(b[i] != 0)
+            if(++i >= b.length) return null;
+        var ret = "";
+        while(++i < b.length) {
+            var c = b[i] & 255;
+            if(c < 128) { // utf-8 decode
+                ret += String.fromCharCode(c);
+            }
+            else if((c > 191) && (c < 224)) {
+                ret += String.fromCharCode(((c & 31) << 6) | (b[i+1] & 63));
+                ++i;
+            }
+            else {
+                ret += String.fromCharCode(((c & 15) << 12) | ((b[i+1] & 63) << 6) | (b[i+2] & 63));
+                i += 2;
+            }
+        }
+        return ret;*/
+        return "";
     }
 
-    static std::vector<long long int> encrypt(const PublicKey* publicKey, const std::string& m)
+    static std::string encrypt(const PublicKey* publicKey,
+                               const std::string& m,
+                               PaddingType paddingType = PaddingType::PKCS_1v1_5)
     {
-        std::vector<long long int> result;
-        std::size_t len = m.size();
-        for (std::size_t i = 0; i < len; ++i) {
-            long long int mi = static_cast<long long int>(m[i]);
-            long long int mienc = encrypt(publicKey, mi);
-            result.push_back(mienc);
+
+        std::string result;
+        if (paddingType == PaddingType::PKCS_1v1_5) {
+            std::map<unsigned long, int> msg = pkcs1pad2(m, (publicKey->n().BitCount() + 7) >> 3);
+            std::for_each(msg.begin(), msg.end(), [&](const std::pair<unsigned long, int>& item) {
+                result.append(std::to_string(item.second));
+            });
+        } else {
+            std::for_each(m.begin(), m.end(), [&](char c) {
+                result.append(std::to_string(static_cast<int>(c)));
+            });
+        }
+
+        std::cout << "padded m = " << result << std::endl;
+        BigInteger bi(result.c_str());
+        BigInteger b5(5);
+
+        std::cout << "b5=" << powerMod(b5, 3, 19) << std::endl;
+
+        std::stringstream ss;
+        ss << powerMod(bi, publicKey->e(), publicKey->n());
+        return ss.str();
+    }
+
+    static std::string decrypt(const PrivateKey* privateKey,
+                               const std::string& m,
+                               PaddingType paddingType = PaddingType::NO_PADDING)
+    {
+        BigInteger msg(m.c_str());
+        BigInteger decr(powerMod(msg, privateKey->d(), privateKey->n()));
+        std::string result;
+
+        if (paddingType == PaddingType::PKCS_1v1_5) {
+            result = pkcs1unpad2(decr, (privateKey->n().BitCount() + 7) >> 3);
+        } else {
+            // todo: convert to chars e.g, 72105 => Hi
+            result = std::to_string(decr.ConvertToLong());
         }
         return result;
-    }
-
-    static std::string decrypt(const PrivateKey* privateKey, const std::vector<long long int>& m)
-    {
-        std::stringstream ss;
-        std::size_t len = m.size();
-        for (std::size_t i = 0; i < len; ++i) {
-            long long int mi = static_cast<long long int>(m[i]);
-            ss << static_cast<char>(decrypt(privateKey, mi));
-        }
-        return ss.str();
     }
 private:
 #if 0
@@ -210,7 +297,8 @@ private:
         return b;
     }
 
-    static long long powerMod(BigInteger b, BigInteger e, BigInteger m) {
+    static BigInteger powerMod(BigInteger b, BigInteger e, BigInteger m)
+    {
         BigInteger res = 1;
         while (e > 0) {
             if (e % 2 != 0) {
@@ -219,11 +307,12 @@ private:
             b = (b * b) % m;
             e /= 2;
         }
-        return res.toLongLong();
+        return res;
     }
 
     static BigInteger modInverse(BigInteger a, BigInteger b)
     {
+        return a.InverseMod(b);/*
         BigInteger b0 = b, t, q;
         BigInteger x0 = 0, x1 = 1;
         if (b == 1) return 1;
@@ -233,7 +322,7 @@ private:
             t = x0, x0 = x1 - q * x0, x1 = t;
         }
         if (x1 < 0) x1 += b0;
-        return x1;
+        return x1;*/
     }
 
     static bool isPrime(BigInteger n)
@@ -250,7 +339,7 @@ private:
         }
         for (BigInteger i = 5; i * i <= n; i += 6) {
             if (n % i == 0 || n % (i + 2) == 0) {
-               return false;
+                return false;
             }
         }
         return true;
